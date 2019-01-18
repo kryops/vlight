611d55860d12b1740bc1a1933972b865529fd2dc
Michael Strobel <michael@kryops.de>
2019-01-18 23:25:23 +0100

    Add icon library and start design baseline

--- frontend/src/experiment/lazy/lazy.tsx
+++ frontend/src/experiment/lazy/lazy.tsx
@@ -1,9 +1,16 @@
+import { mdiAccount } from '@mdi/js'
 import React from 'react'
 
 import styles from './lazy.scss'
 
 const LazyComponent: React.SFC = () => (
-  <div className={styles.lazy}>LAZY LOADED</div>
+  <div className={styles.lazy}>
+    LAZY foo{' '}
+    <svg>
+      <path d={mdiAccount} />
+    </svg>{' '}
+    LOADED
+  </div>
 )
 
 export default LazyComponent
