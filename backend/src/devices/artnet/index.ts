dab98233ae50c6a0c6d6dd2c4032a8dfbfef001e
Michael Strobel <michael@kryops.de>
2019-07-13 10:23:37 +0200

    Log ArtNet channel updates

--- backend/src/devices/artnet/index.ts
+++ backend/src/devices/artnet/index.ts
@@ -2,6 +2,7 @@ import artnet, { Artnet } from 'artnet'
 
 import { artnetHost, enableArtNetDevices } from '../../config'
 import { getDmxUniverse } from '../../universe'
+import { logTrace } from '../../util/log';
 
 let server: Artnet
 
@@ -19,5 +20,6 @@ export function broadcastArtNetChannel(channel: number, value: number) {
   if (!enableArtNetDevices || server === undefined) {
     return
   }
+  logTrace('set ArtNet channel', channel, value)
   server.set(channel, value)
 }
