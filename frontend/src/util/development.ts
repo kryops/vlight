aad85502d6963538d084b4f2943f69181e988274
Michael Strobel <michael@kryops.de>
2019-08-30 17:06:18 +0200

    disable memoInProduction helper

--- frontend/src/util/development.ts
+++ frontend/src/util/development.ts
@@ -1,5 +1,8 @@
 import { memo } from 'react'
 
 export function memoInProduction<T>(component: React.ComponentType<T>) {
-  return process.env.NODE_ENV === 'production' ? memo(component) : component
+  // This was necessary because react-hot-loader did not support updating memo-components.
+  // We leave it in place in case this happens again
+  return memo(component)
+  // return process.env.NODE_ENV === 'production' ? memo(component) : component
 }
