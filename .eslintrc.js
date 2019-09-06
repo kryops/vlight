d21cfa57520d8adbba659dc6815c7504c553282f
Michael Strobel <michael@kryops.de>
2019-09-06 11:49:14 +0200

    Use eslint-plugin-node for backend

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     'prettier/react',
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
