a96d10e05cf3f4d66d02f315d06e3c064ea06202
Michael Strobel <michael.strobel@mobilexag.de>
2020-10-24 12:30:27 +0200

    Upgrade dependencies (React 17)

--- backend/src/util/network.ts
+++ backend/src/util/network.ts
@@ -1,8 +1,9 @@
 import { AddressInfo } from 'net'
 
-export function getAddressString(address: string | AddressInfo): string {
+export function getAddressString(address: string | AddressInfo | {}): string {
   if (typeof address === 'string') {
     return address
   }
+  if (!('address' in address)) return JSON.stringify(address)
   return address.address + ':' + address.port
 }
