9cf220cc00ea148a18074a35645ff1408b672089
Michael Strobel <michael@kryops.de>
2022-06-11 01:00:33 +0200

    Add memory flash button

--- frontend/src/widgets/memory/stateless-memory-widget.tsx
+++ frontend/src/widgets/memory/stateless-memory-widget.tsx
@@ -1,13 +1,27 @@
+import { css } from '@linaria/core'
 import { Memory, MemoryState } from '@vlight/types'
 
 import { setMemoryState } from '../../api'
 import { openEntityEditorForId } from '../../pages/config/entities/editors'
+import { Button } from '../../ui/buttons/button'
 import { Widget } from '../../ui/containers/widget'
 import { Fader } from '../../ui/controls/fader/fader'
 import { faderContainer } from '../../ui/css/fader-container'
-import { iconConfig } from '../../ui/icons'
+import { iconConfig, iconLight } from '../../ui/icons'
 import { Icon } from '../../ui/icons/icon'
+import { baseline } from '../../ui/styles'
 import { memoInProduction } from '../../util/development'
+
+const container = css`
+  display: flex;
+  align-items: flex-start;
+  gap: ${baseline(4)};
+  justify-content: center;
+`
+
+const fader = css`
+  margin: 0 ${baseline(2)};
+`
 
 export interface StatelessMemoryWidgetProps {
   memory: Memory
@@ -30,24 +44,46 @@ export const StatelessMemoryWidget = memoInProduction(
           />
         }
         onTitleClick={() =>
-          setMemoryState(memory.id, {
-            value: state.value,
-            on: !state.on,
-          })
+          setMemoryState(
+            memory.id,
+            {
+              on: !state.on,
+            },
+            true
+          )
         }
         turnedOn={state.on}
       >
-        <div className={faderContainer}>
-          <Fader
-            max={255}
-            step={1}
-            value={state.value ?? 0}
-            onChange={value =>
-              setMemoryState(memory.id, {
-                on: state.on,
-                value,
-              })
+        <div className={container}>
+          <div className={faderContainer}>
+            <Fader
+              className={fader}
+              max={255}
+              step={1}
+              value={state.value ?? 0}
+              onChange={value =>
+                setMemoryState(
+                  memory.id,
+                  {
+                    value,
+                  },
+                  true
+                )
+              }
+            />
+          </div>
+          <Button
+            icon={iconLight}
+            onDown={() =>
+              setMemoryState(
+                memory.id,
+                {
+                  on: true,
+                },
+                true
+              )
             }
+            onUp={() => setMemoryState(memory.id, { on: false }, true)}
           />
         </div>
       </Widget>
