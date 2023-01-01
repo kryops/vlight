48bbd1b480263e0e7e002b35fa1cb43ec96379c7
Michael Strobel <michael@kryops.de>
2023-01-01 18:40:32 +0100

    Fix test

--- backend/test/services/universe.test.ts
+++ backend/test/services/universe.test.ts
@@ -4,25 +4,30 @@
 
 import { broadcastUniverseChannelToApiClients } from '../../src/services/api'
 import { universeSize } from '../../src/services/config'
-import {
-  channelUniverse,
-  init as initChannels,
-} from '../../src/controls/channels'
 import { broadcastUniverseChannelToDevices } from '../../src/devices'
-import { getDmxUniverse, setUniverseChannel } from '../../src/services/universe'
+import {
+  addUniverse,
+  createUniverse,
+  getDmxUniverse,
+  setUniverseChannel,
+} from '../../src/services/universe'
 
 jest.mock('../../src/devices')
 jest.mock('../../src/services/api')
 
 describe('services/universe', () => {
-  beforeAll(() => initChannels())
+  const myUniverse = createUniverse()
+
+  beforeAll(() => {
+    addUniverse(myUniverse)
+  })
 
   it('changes values', () => {
     const mockUniverse = Buffer.alloc(universeSize)
     expect(getDmxUniverse()).toEqual(mockUniverse)
 
     // higher value
-    setUniverseChannel(channelUniverse, 1, 255)
+    setUniverseChannel(myUniverse, 1, 255)
     mockUniverse[0] = 255
     expect(getDmxUniverse()).toEqual(mockUniverse)
 
@@ -30,24 +35,22 @@ describe('services/universe', () => {
     expect(broadcastUniverseChannelToApiClients).toHaveBeenCalledWith(1)
 
     // lower value
-    setUniverseChannel(channelUniverse, 1, 200)
+    setUniverseChannel(myUniverse, 1, 200)
     mockUniverse[0] = 200
     expect(getDmxUniverse()).toEqual(mockUniverse)
     expect(broadcastUniverseChannelToDevices).toHaveBeenLastCalledWith(1, 200)
     expect(broadcastUniverseChannelToApiClients).toHaveBeenLastCalledWith(1)
 
     // same value
-    setUniverseChannel(channelUniverse, 1, 200)
+    setUniverseChannel(myUniverse, 1, 200)
     expect(getDmxUniverse()).toEqual(mockUniverse)
     expect(broadcastUniverseChannelToDevices).toHaveBeenCalledTimes(2)
     expect(broadcastUniverseChannelToApiClients).toHaveBeenCalledTimes(2)
   })
 
   it('throws on invalid channels', () => {
-    expect(() => setUniverseChannel(channelUniverse, -1, 0)).toThrow()
-    expect(() => setUniverseChannel(channelUniverse, 0, 0)).toThrow()
-    expect(() =>
-      setUniverseChannel(channelUniverse, universeSize + 1, 0)
-    ).toThrow()
+    expect(() => setUniverseChannel(myUniverse, -1, 0)).toThrow()
+    expect(() => setUniverseChannel(myUniverse, 0, 0)).toThrow()
+    expect(() => setUniverseChannel(myUniverse, universeSize + 1, 0)).toThrow()
   })
 })
